e2f09dd5fa620068726d25ee9052656ad6f82343
Zyad Mhamoud <61472959+zyadmahmoud3993@users.noreply.github.com>
2025-06-21 13:23:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function startQuiz(index) {
 
         item.addEventListener('click', () => {
             item.querySelector('.icon').classList.add('active');
-            settimeout(()=>{item.classList.remove('hover')},1250)
+            setTimeout(()=>{item.classList.remove('hover')},1250)
             item.textContent.trim() == quiztion_random[index].answer.trim() ? [item.classList.add('true'), curect++, curect_voice.play()] : [item.classList.add('false'), window.navigator.platform.includes('Win') ? error_voice.play() : window.navigator.vibrate[300]];
             quiz_item.forEach(item => {
                 item.style.pointerEvents = 'none';
